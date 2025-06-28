e5696b713a7846d3a2aad52b9796f416a6c31b7f
darkluke210@hotmail.co.uk <darkluke210@hotmail.co.uk>
2025-06-28 14:01:04 +0100

    added char hash

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include <string>
+#include <unordered_map>
 
-int main(int, char**){
-    std::cout << "Hello, from entropy!\n";
+int main(int argc, char**argv ){
+
+    std::string input = "This is a piece of text used to demonstrate Huffman encoding";
+    std::unordered_map<std::string, int> alphabet;
+    for(int i = 0; i < input.size(); i++){
+        alphabet[input.substr(i,1)]++;
+    }
+
+    std::cout << "total chars: " << input.size() << std::endl;
+    for(auto out : alphabet){
+        std::cout << out.first << " | " << out.second << std::endl;
+    }
 }
+
+// Huffman encoding
