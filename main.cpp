4f8e78ece4c58e0062d974d433d9821d4401cdcf
darkluke210@hotmail.co.uk <darkluke210@hotmail.co.uk>
2025-06-29 00:48:27 +0100

    added token merge

--- main.cpp
+++ main.cpp
@@ -13,13 +13,18 @@
 // }
 
 // merge, take the tokenised sequence {tokens}, replace any occurences of pair {a,b} with {idx};
-std::vector<int> merge(std::vector<int> &tokens, int a, int b , int idx){
+std::vector<int> merge(std::vector<int> &tokens, int a, int b , int idx, std::unordered_map<int, int>& freq, std::vector<std::vector<int>>& mergepairs, std::vector<int>& mergetokens){
     std::vector<int> out;
     int i = 0;
+    mergepairs.push_back({a, b}); // add the token merge pair to the new pair map
+    mergetokens.push_back(idx);
     while( i < tokens.size() ){
         if( tokens[i] == a && tokens[i+1] == b && i < tokens.size() - 1 ){
             out.push_back(idx);
             i += 2;
+            freq[idx]++; // housekeeping on freq without rehashing.
+            freq[a]--;
+            freq[b]--;
         }
         else{
             out.push_back(tokens[i]);
@@ -42,16 +47,20 @@ int main(int argc, char**argv ){
     std::string input = "This is a piece of text used to demonstrate Huffman encoding";
     std::cout << "total chars: " << input.size() << std::endl;
     std::unordered_map<std::string, int> alphabet; // count the frequency of each 2 length string
+    std::unordered_map<int, int> freq; // token to frequency
     std::unordered_map<std::string, int> token; // assign a token value to each character
+    std::unordered_map<int, std::string> rtoken; // for easy translation of token value back to each character 
 
     for(int i = 0; i < input.size(); i++){
         alphabet[input.substr(i,2)]++;
         token[input.substr(i,1)] = i;
+        rtoken[i] = input.substr(i,1);
+        freq[i]++;
     }
     
-    typedef std::pair<int, std::string> freq;
+    typedef std::pair<int, std::string> freqs;
 
-    std::priority_queue<freq> q;
+    std::priority_queue<freqs> q;
     for(auto out : alphabet ){
         q.push({out.second, out.first});
     } 
@@ -67,19 +76,22 @@ int main(int argc, char**argv ){
     std::vector<int> tokens;
     for(int i = 0; i < input.size(); i++){
         tokens.push_back(token[input.substr(i, 1)]);
-        std::cout << tokens[i];
+        std::cout << tokens[i] << ", ";
     }
 
     printSeq(tokens);
     // std::vector<int> randomList = {1, 2, 3, 4, 5};
     // randomList = merge(randomList, 4, 5, 99);
     // printSeq(randomList);
+    int num_merges = 10;
+    std::vector<int> mergetokens;
+    std::vector<std::vector<int>> mergepairs;
+    while ( mergetokens.size() < num_merges ){
+        auto max_t = *std::max_element(token.begin(), token.end());
+        tokens = merge(tokens, a, b, idx, )
+    }
     
-    // loop this, include manipulation of the hash maps.
-    int max_p = *std::max_element() 
-    int max_t = *std::max_element(tokens.begin(), tokens.end());
-    tokens = merge(tokens, a, b, max_t + 1);
-
+    // tokens = merge(tokens, a, b, max_t + 1);
     // for(auto out : alphabet){
     //     std::cout << out.first << " | " << out.second << std::endl;
     // }
